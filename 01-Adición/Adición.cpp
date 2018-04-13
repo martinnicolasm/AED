2e5cb26fece68dd183cc5d4c4802d64a5d990287
martinnicolasm <37809641+martinnicolasm@users.noreply.github.com>
2018-04-13 16:51:24 -0300

    Add files via upload

---
+++ 01-Adición/Adición.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main () {
+	int a,b;
+	
+	std::cout<<"Ingrese un valor numerico. \n";
+	std::cin>>a;
+	std::cout<<"Ingrese otro valor numerico \n";
+	std::cin>>b;
+	std::cout<<"Su suma es: \n";
+	std::cout<<a+b;
+}
